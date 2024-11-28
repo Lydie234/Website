49a6dd12aa7826e842a98a3de62dc0e50c862715
Lydie234 <lydiemattithia345@gmail.com>
2024-11-28 16:16:41 +0200

    Add files via upload

---
+++ project.js
@@ -0,0 +1,7 @@
+function myMap() {
+    var mapProp = {
+        center:new google.maps.LatLng(-2.00170,30.08966),
+        zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
